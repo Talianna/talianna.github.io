ea56938a5d481dfc9e662e08090883adf9d3d766
Talianna <nat_joe@mail.ru>
2021-06-25 13:08:29 +0300

    new vue

---
+++ js/closeMenu.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$('.menuLink').click(function () { 
+    $('.mobileMenu').addClass('hidden'); 
+    // $('.overlay').addClass('hidden'); 
+    // showModal.classList.toggle('rotate');
+    $('.mobileMenuButton').removeClass('rotate'); 
+
+
+})
+
---
+++ js/slider.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$('.headSlider').slick({
+
+    prevArrow: '<div class="itemNav__arrow itemNav__arrow_left"></div>',    // навигация стрелки
+    nextArrow: '<div class="itemNav__arrow"></div>',
+
+    appendArrows: $('.sliderItem__nav'),
+
+    dots: true,
+    appendDots: $('.itemNav__bullets'),
+
+    // autoplay: true,
+    autoplaySpeed: 3500
+});
